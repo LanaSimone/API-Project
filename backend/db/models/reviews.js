8129c0435f14e175792efa8a54896876cd3c6e7c
LanaSimone <allanadecarishs@gmail.com>
2023-10-21 16:45:34 -0400

    fixed error

--- backend/db/models/reviews.js
+++ backend/db/models/reviews.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
     spotId: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'Spots',
+        model: 'Spot',
         key: 'id'
       }
     },
