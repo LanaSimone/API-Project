3c5413fe4d1eb95feb7773c79e61f0f1565f05b1
LanaSimone <allanadecarishs@gmail.com>
2023-10-21 20:02:15 -0400

    added package.json

--- backend/db/migrations/3-create-spot.js
+++ backend/db/migrations/3-create-spot.js
@@ -1,72 +1,72 @@
-// 'use strict';
+'use strict';
 
 
-// let options = {};
-// if (process.env.NODE_ENV === 'production') {
-//   options.schema = process.env.SCHEMA;  // define your schema in options object
-// }
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     await queryInterface.createTable('Spots', {
-//       id: {
-//         allowNull: false,
-//         autoIncrement: true,
-//         primaryKey: true,
-//         type: Sequelize.INTEGER
-//       },
-//       ownerId: {
-//         type: Sequelize.INTEGER,
-//         references: {
-//           model: 'Users',
-//           key: 'id'
-//         }
-//       },
-//       address: {
-//         type: Sequelize.STRING(100),
-//         allowNull: false
-//       },
-//       city: {
-//         type: Sequelize.STRING(100)
-//       },
-//       state: {
-//         type: Sequelize.STRING(100),
-//         allowNull: false,
-//       },
-//       country: {
-//         type: Sequelize.STRING(100),
-//         allowNull: false
-//       },
-//       lat: {
-//         type: Sequelize.DECIMAL,
-//         allowNull: false,
-//       },
-//       lng: {
-//         type: Sequelize.DECIMAL,
-//         allowNull: false,
-//       },
-//       name: {
-//         type: Sequelize.STRING(100)
-//       },
-//       description: {
-//         type: Sequelize.STRING(200)
-//       },
-//       price: {
-//         type: Sequelize.DECIMAL,
-//         allowNull: false,
-//       },
-//       createdAt: {
-//         allowNull: false,
-//         type: Sequelize.DATE
-//       },
-//       updatedAt: {
-//         allowNull: false,
-//         type: Sequelize.DATE
-//       }
-//     }, options)
-//   },
-//   async down(queryInterface, Sequelize) {
-//     options.tableName = 'Spots'
-//     await queryInterface.dropTable('Spots');
-//   }
-// };
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      address: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING(100)
+      },
+      state: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      country: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      lat: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      lng: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(100)
+      },
+      description: {
+        type: Sequelize.STRING(200)
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, options)
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Spots'
+    await queryInterface.dropTable('Spots');
+  }
+};
