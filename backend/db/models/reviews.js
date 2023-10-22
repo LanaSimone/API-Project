451d472a1e905f0a425d2e7efd34cf18d72cc86b
LanaSimone <allanadecarishs@gmail.com>
2023-10-21 20:15:03 -0400

    added package.json

--- backend/db/models/reviews.js
+++ backend/db/models/reviews.js
@@ -1,59 +1,59 @@
-// 'use strict';
-// const { Model, Validator } = require('sequelize');
+'use strict';
+const { Model, Validator } = require('sequelize');
 
-// module.exports = (sequelize, DataTypes) => {
-//   class Review extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//       Review.belongsTo(models.User, { // Reference 'User', not 'Users'
-//         foreignKey: 'userId',
-//         onDelete: 'CASCADE',
-//       });
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Review.belongsTo(models.User, { // Reference 'User', not 'Users'
+        foreignKey: 'userId',
+        onDelete: 'CASCADE',
+      });
 
-//       Review.belongsTo(models.Spots, { // Reference 'Spot', not 'Spots'
-//         foreignKey: 'spotId',
-//         onDelete: 'CASCADE',
-//       });
-//       Review.hasMany(models.ReviewImage, {
-//         foreignKey: 'reviewId', // The foreign key in the ReviewImages model
-//         onDelete: 'CASCADE',
-//       });
-//     }
-//   }
-//   Review.init({
-//     spotId: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: 'Spots',
-//         key: 'id'
-//       }
-//     },
-//     userId: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: 'Users',
-//         key: 'id'
-//       }
-//     },
-//     review: {
-//       type: DataTypes.STRING(500)
-//     },
-//     createdAt: {
-//       type: DataTypes.DATE,
-//       defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-//     },
-//     updatedAt: {
-//       type: DataTypes.DATE,
-//       defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-//     },
-//   }, {
-//     sequelize,
-//     modelName: 'Review',
-//   });
-//   return Review;
-// };
+      Review.belongsTo(models.Spots, { // Reference 'Spot', not 'Spots'
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+      });
+      Review.hasMany(models.ReviewImage, {
+        foreignKey: 'reviewId', // The foreign key in the ReviewImages model
+        onDelete: 'CASCADE',
+      });
+    }
+  }
+  Review.init({
+    spotId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Spots',
+        key: 'id'
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    review: {
+      type: DataTypes.STRING(500)
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
--- backend/db/models/spotImage.js
+++ backend/db/models/spotImage.js
@@ -1,50 +1,50 @@
-// 'use strict';
-// const { Model, Validator, Sequelize } = require('sequelize');
+'use strict';
+const { Model, Validator, Sequelize } = require('sequelize');
 
 
-// module.exports = (sequelize, DataTypes) => {
-//   class SpotImage extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       SpotImage.belongsTo(models.Spots, { // Reference 'User', not 'Users'
-//         foreignKey: 'spotId',
-//         onDelete: 'CASCADE',
-//       });
-//     }
-//   }
-//   SpotImage.init({
-//     spotId: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       references: {
-//         model: 'Spots',
-//         key: 'id'
-//       },
-//     },
-//     url: {
-//       type: DataTypes.STRING(500),
-//       allowNull: false,
-//     },
-//     preview: {
-//       type: DataTypes.BOOLEAN,
-//       allowNull: false,
-//     },
-//     createdAt: {
-//       type: DataTypes.DATE,
-//       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), // Capital 'S' for Sequelize
-//     },
-//     updatedAt: {
-//       type: DataTypes.DATE,
-//       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), // Capital 'S' for Sequelize
-//     },
+module.exports = (sequelize, DataTypes) => {
+  class SpotImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      SpotImage.belongsTo(models.Spots, { // Reference 'User', not 'Users'
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+      });
+    }
+  }
+  SpotImage.init({
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Spots',
+        key: 'id'
+      },
+    },
+    url: {
+      type: DataTypes.STRING(500),
+      allowNull: false,
+    },
+    preview: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), // Capital 'S' for Sequelize
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), // Capital 'S' for Sequelize
+    },
 
-//   },{
-//     sequelize,
-//     modelName: 'SpotImage',
-//   });
-//   return SpotImage;
-// };
+  },{
+    sequelize,
+    modelName: 'SpotImage',
+  });
+  return SpotImage;
+};
