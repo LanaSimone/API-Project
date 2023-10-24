8556a81b4170b39915db286cd588ceb6d9bc12d9
LanaSimone <allanadecarishs@gmail.com>
2023-10-24 19:46:32 -0400

    created review seeder data

---
+++ backend/db/seeders/20231024233438-create-review-data.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Reviews', [
+      {
+        userId: 1, // Replace with the actual userId
+        spotId: 1, // Replace with the actual spotId
+        review: 'This is a sample review.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 2, // Replace with the actual userId
+        spotId: 2, // Replace with the actual spotId
+        review: 'This is a sample review.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 3, // Replace with the actual userId
+        spotId: 3, // Replace with the actual spotId
+        review: 'This is a sample review.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Reviews', null, {});
+  },
+};
