39f3a8135b05bc0733825e809d86029e1c6f397d
LanaSimone <allanadecarishs@gmail.com>
2023-10-31 00:50:06 -0400

    test

--- backend/db/migrations/5-add-validation-constraints-to-spot.js
+++ backend/db/migrations/5-add-validation-constraints-to-spot.js
@@ -12,28 +12,53 @@ module.exports = {
     await queryInterface.changeColumn('Spots', 'address', {
       type: Sequelize.STRING(100),
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Street address is required',
+        },
+      },
     });
 
     await queryInterface.changeColumn('Spots', 'city', {
       type: Sequelize.STRING(100),
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'City is required',
+        },
+      },
     });
 
     await queryInterface.changeColumn('Spots', 'state', {
       type: Sequelize.STRING(100),
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'State is required',
+        },
+      },
     });
 
     await queryInterface.changeColumn('Spots', 'country', {
       type: Sequelize.STRING(100),
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Country is required',
+        },
+      },
     });
 
     await queryInterface.changeColumn('Spots', 'lat', {
       type: Sequelize.DECIMAL,
       allowNull: false,
       validate: {
-        min: -90,
-        max: 90,
+        notNull: {
+          msg: 'Latitude is not valid',
+        },
+        isFloat: {
+          msg: 'Latitude must be a valid number',
+        },
       },
     });
 
@@ -41,25 +66,50 @@ module.exports = {
       type: Sequelize.DECIMAL,
       allowNull: false,
       validate: {
-        min: -180,
-        max: 180,
+        notNull: {
+          msg: 'Longitude is not valid',
+        },
+        isFloat: {
+          msg: 'Longitude must be a valid number',
+        },
       },
     });
 
     await queryInterface.changeColumn('Spots', 'name', {
       type: Sequelize.STRING(100),
+      allowNull: false,
       validate: {
-        len: [1, 50], // Name must be between 1 and 50 characters.
+        notNull: {
+          msg: 'Name is required',
+        },
+        len: {
+          args: [1, 50],
+          msg: 'Name must be less than 50 characters',
+        },
       },
     });
 
     await queryInterface.changeColumn('Spots', 'description', {
       type: Sequelize.STRING(200),
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Description is required',
+        },
+      },
     });
 
     await queryInterface.changeColumn('Spots', 'price', {
       type: Sequelize.DECIMAL,
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Price per day is required',
+        },
+        isFloat: {
+          msg: 'Price must be a valid number',
+        },
+      },
     });
   },
 
