a837cb20ce9ee21bb7131ed61399c6944ae4d4f0
LanaSimone <allanadecarishs@gmail.com>
2023-11-04 14:18:39 -0400

    test

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -972,6 +972,10 @@ router.post('/:spotId/bookings', requireAuth, async (req, res) => {
       return res.status(404).json({ message: "Spot couldn't be found" });
     }
 
+    // Add a check to ensure the spot belongs to the user
+    if (spot.ownerId !== userId) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
     // Convert the input dates to Date objects
     const startDateObj = new Date(startDate);
     const endDateObj = new Date(endDate);
