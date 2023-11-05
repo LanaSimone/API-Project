f16246dcf2c61e6d9af388412bb02e33e8280481
LanaSimone <allanadecarishs@gmail.com>
2023-11-05 18:30:40 -0500

    test

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -51,6 +51,22 @@ const requireSpotOwnership = require('../api/spots')
 //   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 // };
 
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+function formatDateTime(dateTime) {
+  const date = formatDate(dateTime);
+  const hours = dateTime.getHours().toString().padStart(2, '0');
+  const minutes = dateTime.getMinutes().toString().padStart(2, '0');
+  const seconds = dateTime.getSeconds().toString().padStart(2, '0');
+  return `${date} ${hours}:${minutes}:${seconds}`;
+}
+
+
 router.get('/current', requireAuth, async (req, res) => {
   try {
     const userId = req.user.id;
@@ -74,15 +90,6 @@ router.get('/current', requireAuth, async (req, res) => {
       const { ownerId, address, city, state, country, lat, lng, name, price, SpotImages } = Spot;
       const previewImage = SpotImages[0] ? SpotImages[0].url : ''; // Assuming you want the first URL
 
-      const formatDate = (date) => {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-      };
 
 
       return {
@@ -102,10 +109,10 @@ router.get('/current', requireAuth, async (req, res) => {
           previewImage,
         },
         userId,
-        startDate: formatDate(new Date(startDate)),
-        endDate: formatDate(new Date(endDate)),
-        createdAt: formatDate(new Date(createdAt)),
-        updatedAt: formatDate(new Date(updatedAt)),
+        startDate: formatDate(startDate), // Format the startDate
+        endDate: formatDate(endDate), // Format the endDate
+        createdAt: formatDateTime(createdAt), // Format the createdAt
+        updatedAt: formatDateTime(updatedAt), // Format the updatedAt
       };
     });
 
